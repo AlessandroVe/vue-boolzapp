716da85dc2ca3dd8d0eda38854ae3127b80d328c
AlessandroVe <aveneziano536@gmail.com>
2021-07-01 11:25:15 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const app =  new Vue({
     el:"#app",
     data:{
+        user:{
+            name:"Nome",
+            surname:"Utente",
+            avatar:'./img/avatar_io.jpg',
+        },
         contacts: [
             {
                 name: 'Michele',
